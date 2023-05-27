5b9a88a3c61b658bdd451404224700d81a90248b
JuanDiegoReyes <130397141+JuanDiegoReyes@users.noreply.github.com>
2023-05-26 18:54:04 -0600

    Add files via upload

---
+++ Portafolio/Proyecto/Scripts/cargarPagina.js
@@ -0,0 +1,6 @@
+function cargarPag(htmlDocument) {
+	document.getElementById("content").innerHTML = '<object width=\"75%\" height=\"75%\" type="text/html" data="' + htmlDocument + '" ></object>';
+}
+function contarpuntos(valor){
+	
+}
